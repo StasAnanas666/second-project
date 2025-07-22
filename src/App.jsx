331afe2225255f596d38ffd0aa9093c9c5673059
StasAnanas666 <lapshin-5a@yandex.ru>
2025-07-22 20:23:54 +0300

    final version

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 function App() {
     //массив задач, по умолчанию пустой массив
-    //это глобальное состояние, которое будет использоваться дргими компонентами
-    const [tasks, setTasks] = useState([]);
+    //это глобальное состояние, которое будет использоваться другими компонентами
+    const [tasks, setTasks] = useState(() => {
+        //загружаем задачи из localStorage
+        const savedTasks = localStorage.getItem("tasks");
+        //если данные в LocalStorage есть, иначе - пустой массив
+        return savedTasks ? JSON.parse(savedTasks) : []
+    });
+
+    useEffect(() => {
+        setTasks(JSON.parse(localStorage.getItem("tasks")));
+    }, [])
+
+    //вызывается при изменении состояния tasks
+    useEffect(() => {
+        console.log("Сработал useEffect");
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
+
 
     return (
         <div className="container my-3">
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,18 @@
 import TodoItem from "./TodoItem";
 
-function TodoList({tasks, setTasks}) {
+function TodoList({ tasks, setTasks }) {
     return (
         <ul className="list-group my-4">
-            {
-                tasks.map(task => (
-                    <TodoItem task={task} setTasks={setTasks} tasks={tasks} />
-                ))
-            }
+            {tasks.map((task) => (
+                <TodoItem
+                    key={task.id}
+                    task={task}
+                    setTasks={setTasks}
+                    tasks={tasks}
+                />
+            ))}
         </ul>
-    )
+    );
 }
 
 export default TodoList;
